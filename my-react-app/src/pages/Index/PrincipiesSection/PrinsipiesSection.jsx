08bc87f97c450f868a90228ad2e0736d23d8a465
reborn-type <ruslanh593@gmail.com>
2026-04-29 21:24:07 +0300

    feat(pages): add Sections and Add IndexPage, feat(img): add folder img and abstract image, style(container): switch text to white and reduce padding/margin

---
+++ my-react-app/src/pages/Index/PrincipiesSection/PrinsipiesSection.jsx
@@ -0,0 +1,16 @@
+import Principe from "@components/ui/containers/Principe/Principe";
+import styles from './PrincipiesSection.module.css';
+
+const PrincipiesSection = () => {
+
+    return (
+        <div className={styles.principies__container}>
+            <h2 className={styles.principies__header}>Принцип работы:</h2>
+            <Principe title={`Формирование`} text={`Создать обращение можно на странице “заявления”`} num={'1'}/>
+            <Principe title={`Обработка`} text={`Обращение обрабатывается на стороне муниципалитета, сроки решения зависят от нагрузки на портал, и сложности задачи.`} num={'2'}/>
+            <Principe title={`Ответ`} text={`Ответ приходит в чате, после решения проблемы, обращение переходит в статус “Закрыто”, решение сохраняется.`} num={'3'}/>
+        </div>
+    )
+}
+
+export default PrincipiesSection
