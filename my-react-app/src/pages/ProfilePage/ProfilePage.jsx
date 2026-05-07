98795da9ff3d5b64f7f85ca893beb371f464071c
reborn-type <ruslanh593@gmail.com>
2026-05-07 16:55:26 +0300

    refactored(ProfilePage): changed buttons

--- my-react-app/src/pages/ProfilePage/ProfilePage.jsx
+++ my-react-app/src/pages/ProfilePage/ProfilePage.jsx
@@ -5,6 +5,12 @@ import ProfileMockPic from "@p/mock/ProfileMockPic.png";
 import mailIcon from "@icons/mail.svg";
 import groupIcon from "@icons/group.svg";
 import locationIcon from "@icons/location.svg";
+import closedIcn from "@icons/closed.svg";
+import inWorkIcn from "@icons/in_work.svg";
+import logoutIcn from "@icons/logout.svg";
+import penIcn from "@icons/pen_8x8.svg";
+import telegramIcn from "@icons/telegram.svg";
+import YellowBtn from '../../components/ui/buttons/YellowBtn/YellowBtn';
 
 const ProfilePage = () => {
   return (
@@ -18,7 +24,7 @@ const ProfilePage = () => {
 
       <div className={classes.card}>
         <div className={classes.infoRow}>
-          <img src={groupIcon} className={classes.charIcon} alt="group" />
+          <img src={locationIcon} className={classes.charIcon} alt="group" />
           <span>ГБУ Жилищник Донского района</span>
         </div>
         <div className={classes.infoRow}>
@@ -41,41 +47,20 @@ const ProfilePage = () => {
           <div className={`${classes.card} ${classes.statsBox}`}>
             <h3>Статистика:</h3>
             <div className={classes.statItem}>
-              <svg className={classes.icon} viewBox="0 0 14 10" fill="none" stroke="white" strokeWidth="2">
-                <path d="M1 5L5 9L13 1" />
-              </svg>
+              <img src={closedIcn} alt="Закрыто" />
               Закрыто заявок: 12
             </div>
             <div className={classes.statItem}>
-              <svg className={classes.icon} viewBox="0 0 14 14" fill="none" stroke="white" strokeWidth="1.5">
-                <rect x="2" y="2" width="10" height="10" rx="1" />
-                <path d="M4 5H10M4 7H10M4 9H7" />
-              </svg>
+              <img src={inWorkIcn} alt="В работе" />
               Заявок в работе: 0
             </div>
           </div>
         </div>
 
         <div className={classes.buttonGroup}>
-          <button className={classes.mainButton}>
-            <svg className={classes.icon} viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="3">
-              <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
-            </svg>
-            Сменить пароль
-          </button>
-          <button className={classes.mainButton}>
-            <svg className={classes.icon} viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="3">
-              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-              <circle cx="12" cy="12" r="3" />
-            </svg>
-            Подключить
-          </button>
-          <button className={`${classes.mainButton} ${classes.btnExit}`}>
-            <svg className={classes.icon} viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="3">
-              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9" />
-            </svg>
-            Выйти
-          </button>
+          <YellowBtn pathSvg={penIcn}>Сменить пароль</YellowBtn>
+          <YellowBtn pathSvg={telegramIcn}>Подключить</YellowBtn>
+          <YellowBtn pathSvg={logoutIcn}>Выйти</YellowBtn>
         </div>
       </div>
     </div>
